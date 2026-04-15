f6bb44119795cf9f1c85af26bbe1ea90090fdd9e
unknown <prabhnoorsingh85@gmail.com>
2026-04-15 12:38:32 +0200

    learning about variables

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+#comments in python or any other code is to explain the code to you and anyone else who might want to update it in the future
 print ("I like Pizza.")
 print ("It's really tasty.")
+
+#Variables
+First_name = "Prabhnoor "
+
+print(f"Hello {First_name}")
